de2077b7644103c026490967b98ab5e47f971923
Maxime Lelong <maximelelong3@gmail.com>
2023-12-15 16:47:02 +0100

    Fix teleop.launch + SLAM

--- src/myteleop/script/teleop.py
+++ src/myteleop/script/teleop.py
@@ -1,18 +1,30 @@
+#! /usr/bin/env python3
 import rospy
-
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+import sys
 
 class Teleop:
     def __init__(self):
         self._pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
         self._sub = rospy.Subscriber('joy', Joy, self._callback)
-        self._twist = Twist()
 
     def _callback(self, joy_msg):
-        self._twist.linear.x = joy_msg.axes[1]
-        self._twist.angular.z = joy_msg.axes[0]
+        new_twist = Twist()
+        new_twist.linear.x = joy_msg.axes[1]
+        new_twist.angular.z = joy_msg.axes[0]
+        self._pub.publish(new_twist)
+
 
     def spin(self):
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
-            self._pub.publish(self._twist)
             rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('teleop')
+    try:
+        teleop = Teleop()
+        teleop.spin()
+    except rospy.ROSInterruptException:
+        print("program interrupted before completion", file=sys.stderr)
